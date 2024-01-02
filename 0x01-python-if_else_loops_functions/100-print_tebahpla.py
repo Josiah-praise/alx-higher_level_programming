269cc769f5e7602555c6d67c1730a80bb4ff4a65
josiah-praise <praisechinonso21@gmail.com>
2024-01-02 19:46:08 +0100

    fixed

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -4,9 +4,9 @@ def reverse_print() -> None:
     """prints the alphabets in reverse"""
     for _ in range(122, 96, -1):
         if _ % 2 == 0:
-            print(chr(_), end='')
+            print('{}'.format(chr(_)), end='')
         else:
-            print(chr(_-32), end='')
+            print('{}'.format(chr(_-32)), end='')
 
 
 if __name__ == '__main__':
