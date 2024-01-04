ff643fddb2ca0b9ec5f5215fec37f837a9773aec
josiah-praise <praisechinonso21@gmail.com>
2024-01-04 14:47:21 +0100

    fixed

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,7 +4,7 @@ from sys import argv
 if __name__ == "__main__":
     argc = len(argv)
     if argc == 1:
-        print('{0} arguments'.format(argc - 1))
+        print('{0} arguments.'.format(argc - 1))
     elif argc == 2:
         print('{0} argument:'.format(argc - 1))
     elif argc > 2:
@@ -12,6 +12,7 @@ if __name__ == "__main__":
 
     count = 1
     if argc > 1:
-        while count < argc:
-            print('{0}: {1}'.format(count, argv[count]))
-            count += 1
+        for _ in argv:
+            if count < argc:
+                print('{0}: {1}'.format(count, argv[count]))
+                count += 1
