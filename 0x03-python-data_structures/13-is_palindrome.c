f0fc42b3ebbff4b85822cb1d301ce874688594ef
josiah-praise <praisechinonso21@gmail.com>
2024-01-08 11:24:24 +0100

    fixed

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,7 +1,59 @@
 #include "lists.h"
+/**
+ * reverse - reverses a linked list
+ * @head: pointer to head
+ * Return: pointer to head
+ */
+listint_t *reverse(listint_t **head){
+	if (head == NULL || *head == NULL)
+		return (NULL);
+
+	listint_t *current, *previous, *next;
+
+	current = *head;
+	previous = NULL;
+
+	while (current){
+		next = current->next;
+		current->next = previous;
+		previous = current;
+		current = next;
+	}
+	*head = previous;
+	return (*head);
+}
 
 /**
  * is_palindrome - checks if a singly linked list is a palindrome
  * @head: pointer to the head
  * Return: 1 if it's a palindrome and 0 if it isn't
  */
+int is_palindrome(listint_t **head)
+{
+	if (head == NULL || *head == NULL)
+		return (0);
+
+	listint_t *fast, *slow;
+
+	slow = *head;
+	fast = *head;
+	while(fast && fast->next)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+	}
+	if (fast)
+		slow = slow->next;
+
+	slow = reverse(&slow);
+
+	fast = *head;
+	while(slow)
+	{
+		if (slow->n != fast->n)
+			return (0);
+		slow = slow->next;
+		fast = fast->next;
+	}
+	return (1);
+}
--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -16,11 +16,10 @@ int n;
 struct listint_s *next;
 } listint_t;
 
-
+int is_palindrome(listint_t **head);
 size_t print_listint(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
 void free_listint(listint_t *head);
 int check_cycle(listint_t *list);
-
 
 #endif /* LISTS_H */
