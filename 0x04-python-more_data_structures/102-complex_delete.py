d4914a9e7dd626b79fc0470e1317ad25c23d0b5e
josiah-praise <praisechinonso21@gmail.com>
2024-01-11 07:19:52 +0100

    first commit

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -2,6 +2,5 @@
 def complex_delete(a_dictionary, value):
     """ deletes keys with a specific value in a dictionary"""
     keys = [key for key, _value in a_dictionary.items() if value == _value]
-    print(keys)
     list(map(lambda x: a_dictionary.pop(x), keys))
     return a_dictionary
