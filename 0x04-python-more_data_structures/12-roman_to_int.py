6ea98e99a550a2de37f793ac24bf96ca22828a78
josiah-praise <praisechinonso21@gmail.com>
2024-01-12 12:59:05 +0100

    first commit

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,1 +1,47 @@
 #!/usr/bin/python3
+roman_keys = {
+    'I': 0,
+    'V': 1,
+    'X': 2,
+    'L': 3,
+    'C': 4,
+    'D': 5,
+    'M': 6,
+}
+
+roman_values = {
+    'I': 1,
+    'V': 5,
+    'X': 10,
+    'L': 50,
+    'C': 100,
+    'D': 500,
+    'M': 1000,
+}
+
+
+def roman_to_int(roman_string: str) -> int:
+    """converts roman numeral to string
+    :param roman_string - roman numeral
+    :return roman numeral as integer
+    """
+    value = 0
+    if not isinstance(roman_string, str):
+        return 0
+    for idx, char in enumerate(roman_string):
+        # check if it's a valid char
+        if roman_keys.get(char.upper(), -1) == -1:
+            return 0
+        # check whether you're at the end of the string
+        if idx != (len(roman_string) - 1):
+            # determines whether to do positive or negative addition
+            current_char_weight = roman_keys.get(char.upper(), -1)
+            next_char_weight = roman_keys[roman_string[idx + 1].upper()]
+            intRepOfChar = roman_values[char.upper()]
+            if current_char_weight >= next_char_weight:
+                value += intRepOfChar
+            else:
+                value -= intRepOfChar
+        else:
+            value += roman_values[char.upper()]
+    return value
