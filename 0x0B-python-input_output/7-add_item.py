1ccf892ac53c2608e64c169ba3ce80ec6dd9b6c9
josiah-praise <praisechinonso21@gmail.com>
2024-02-06 16:44:50 +0100

    first commit

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -6,7 +6,7 @@ from sys import argv
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-json_filename = "add.json"
+json_filename = "add_item.json"
 json_list = []
 
 if len(argv) == 1:
