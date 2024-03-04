61e9290622b9ff66011c2b031a8c05113952e275
josiah-praise <praisechinonso21@gmail.com>
2024-03-04 19:03:44 +0100

    tests

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -4,8 +4,8 @@
 
 def add_integer(a, b=98):
     """add two integers"""
-    if not isinstance(a, (float, int)):
+    if ((not isinstance(a, int) and not isinstance(a, float))):
         raise TypeError("a must be an integer")
-    if not isinstance(b, (float, int)):
+    if ((not isinstance(b, int) and not isinstance(b, float))):
         raise TypeError("b must be an integer")
-    return int(a) + int(b)
+    return (int(a) + int(b))
--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,41 +1,21 @@
 #!/usr/bin/python3
 """Divide a matrix"""
 
-def __check_float_int_list(matrix):
-    """checks if matrix is a list of lists of floats or integers"""
-    error = TypeError("matrix must be" \
-    " a matrix (list of  lists) of integers/floats")
-    # check if matrix contains only lists
-    if matrix is None:
-        raise error
-    if not all(isinstance(_, list) for _ in matrix):
-        raise error
+def matrix_divided(matrix, div):
+    if (not isinstance(matrix, list) or matrix == [] or
+            not all(isinstance(row, list) for row in matrix) or
+            not all((isinstance(ele, int) or isinstance(ele, float))
+                    for ele in [num for row in matrix for num in row])):
+        raise TypeError("matrix must be a matrix (list of lists) of "
+                            "integers/floats")
 
-    for _ in matrix: #traverse the matrix
-        for value in _: #traverse the inner list
-            if not isinstance(value, (int, float)):
-                raise error
-
-def __check_matrix_size(matrix):
-    """checks if the rows are of the same size"""
-    length = {len(_) for _ in matrix}
-    if len(length) != 1:
+    if not all(len(row) == len(matrix[0]) for row in matrix):
         raise TypeError("Each row of the matrix must have the same size")
 
-def __check_div(div):
-    """check if div is an int or a float"""
-    if not isinstance(div, (float, int)):
+    if not isinstance(div, int) and not isinstance(div, float):
         raise TypeError("div must be a number")
 
-def __check_zero_division(div):
-    """check if div is 0"""
     if div == 0:
         raise ZeroDivisionError("division by zero")
 
-def matrix_divided(matrix, div) -> list:
-    """divides all elements of a matrix"""
-    __check_float_int_list(matrix)
-    __check_matrix_size(matrix)
-    __check_div(div)
-    __check_zero_division(div)
-    return [[round((_/div), 2) for _ in list_] for list_ in matrix]
+    return ([list(map(lambda x: round(x / div, 2), row)) for row in matrix])
