6dfb5ddf6f0e3c81bded086c522b4972c9e6c11a
josiah-praise <praisechinonso21@gmail.com>
2024-04-08 15:40:14 +0100

    6-multi_languages_loop.js

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+const line1 = 'C is fun';
+const line2 = 'Python is cool';
+const line3 = 'JavaScript is amazing';
+const arr = [line1, line2, line3];
+for (let i of arr) {
+    console.log(i);
+}
