ce27fb3785e7bf9afe494959b062d498ca1ac98b
josiah-praise <praisechinonso21@gmail.com>
2024-04-10 21:43:29 +0100

    fixed errors

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,12 +1,8 @@
 #!/usr/bin/node
 
-const Rectangle = require('./4-rectangle');
+const Rectangle = require('./5-rectangle');
 
 module.exports = class Square extends Rectangle {
-  constructor (side) {
-    super(side, side);
-  }
-
   charPrint (char = 'X') {
     for (let i = 0; i < this.height; i++) {
       console.log(char.repeat(this.width));
