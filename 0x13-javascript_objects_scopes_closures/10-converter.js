116f2445de6854996aa692d0b7fc0767d806c7dc
josiah-praise <praisechinonso21@gmail.com>
2024-04-10 21:46:30 +0100

    10-converter.js

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,14 +1,5 @@
 #!/usr/bin/node
 
-exports.converter = function (base) {
-  function doer (num) {
-    if (Math.floor(num / base) === 0) {
-      return (num % base).toString();
-    } else {
-      const mod = num % base;
-      const quocient = Math.floor(num / base);
-      return doer(quocient) + mod.toString();
-    }
-  }
-  return doer;
+exports.converter = (base) => {
+  return (num) => num.toString(base);
 };
