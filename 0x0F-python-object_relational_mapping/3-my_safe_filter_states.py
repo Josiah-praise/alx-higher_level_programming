d75bbe643bd7b3821e0b419d2deff5f1778263d8
josiah-praise <praisechinonso21@gmail.com>
2024-04-13 09:51:34 +0100

    3-my_safe_filter_states.py

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+'''
+lists all states with a name that matches the
+argument given from the database
+'''
+
+
+def filter_states():
+    '''
+    list all states from database hbtn_0e_0_usa
+    '''
+
+    import MySQLdb
+    import sys
+
+    db = MySQLdb.connect(
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        host='localhost',
+        db=sys.argv[3],
+        port=3306)
+
+    with db.cursor() as cursor:
+        cursor.execute("SELECT * FROM states\
+            WHERE name LIKE BINARY %s ", (sys.argv[4],))
+        for row in cursor.fetchall():
+            print(row)
+
+
+if __name__ == '__main__':
+    filter_states()
