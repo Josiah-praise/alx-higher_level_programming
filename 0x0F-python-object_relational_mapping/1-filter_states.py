81d312545d3de73379f0511e5d6216d97a63742e
josiah-praise <praisechinonso21@gmail.com>
2024-04-13 08:56:44 +0100

    fixed bugs

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -21,7 +21,7 @@ def list_all_states():
         port=3306)
 
     with db.cursor() as cursor:
-        cursor.execute("SELECT * FROM states WHERE name LIKE BINARY'T%'")
+        cursor.execute("SELECT * FROM states WHERE name LIKE BINARY 'T%'")
 
         for row in cursor.fetchall():
             print(row)
